4fd14c5a2e359f0b67a4a3ff6db82a7422b7f01c
tina8860035 <52558635+tina8860035@users.noreply.github.com>
2020-05-14 00:19:14 +0800

    Lesson06

---
+++ Lesson06/NewsJson.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+news_list = []
+url = 'http://oldpaper.g0v.ronny.tw/index/json?fbclid=IwAR1zip-1_wLBrsZ2p9RfiAjm-WeFaxb8UyI4nA-uXOhK6Q3Wkgn7D_Zukow'
+data = json.loads(requests.get(url).text)
+
+for d in data.get('data'):
+    dict = {'title':d.get('title'), 'headlines':d.get('headlines')}
+    news_list.append(dict)
+
+file = open('news4.txt', 'a', encoding='UTF-8')
+for news in news_list:
+    for head in news['headlines']:
+        if '譚德塞' in head[1]:
+            print(head)
+            file.writelines(head)
+            file.write("\n")
---
+++ Lesson06/WriteFile.py
@@ -0,0 +1,3 @@
+file = open('salary.txt', 'w')  # w 覆蓋寫入, a 增加
+file.write('Jane, 80000\n')
+print("OK")
